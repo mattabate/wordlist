45609fc94c025250499583afd5d268a31fcef9ec
Matt Abate <mabate13@gmail.com>
2025-01-28 11:22:10 -0500

    training first db svm model, add clues

--- scripts/update_clues.py
+++ scripts/update_clues.py
@@ -28,12 +28,18 @@ def main():
     )
     rows = [row[0] for row in cur.fetchall()]
 
+    found_so_far = 0
     # 3. Loop and update each missing word
     for word in tqdm(rows, desc="Updating missing clues"):
         if update_clues_for_word(conn, word):
-            tqdm.write(f"{c_green}Added{c_end} clues for word '{word}'.")
+            tqdm.write(
+                f"{c_green}Added{c_end} clues for word '{word}'. Found so far: {found_so_far}"
+            )
+            found_so_far += 1
         else:
-            tqdm.write(f"{c_yellow}Failed{c_end} to update clues for word '{word}'.")
+            tqdm.write(
+                f"{c_yellow}Failed{c_end} to update clues for word '{word}'. Found so far: {found_so_far}"
+            )
 
     # 4. Cleanup
     conn.close()
